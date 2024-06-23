f1f7860fe24f44513461bfb45592d818c47b5ad4
huydv8297 <42988687+huydv8297@users.noreply.github.com>
2024-06-24 01:20:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 console.log("Start proxy server");
 
 app.use('/', createProxyMiddleware({
-    target: 'https://www.example.com', // Change target URL here
+    target: 'https://www.google.com', // Change target URL here
     changeOrigin: true,
 }));
 
